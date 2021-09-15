5347435d780684e1da97d4316fadc69193197fe9
AndyAad <83137173+AndyAad@users.noreply.github.com>
2021-09-16 03:35:06 +0530

    a
    a

---
+++ Tools/Assets/Script.js
@@ -0,0 +1,13 @@
+function(){
+     $('#Header').load('/Tools/Assets/Header.html');
+  })
+
+  $(function(){
+     $('#Footer').load('/rb/Tools/Assets/Footer.html');
+  })
+</script>
+
+
+
+
+document.getElementById("myFooter").innerHTML ="/Tools/Assets/Header.html"
